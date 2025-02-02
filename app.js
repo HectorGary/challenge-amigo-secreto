a38dc3b8ce1ad463d53256b6f62b3251e28bd663
Hector Gary <hector-gary@hotmail.com>
2025-02-01 23:51:10 -0400

    Elimine la funcion limpiarInput

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ function agregarAmigo() {
         }
     } else {
         listaNombreAmigos.push(document.getElementById('amigo').value);
-        //document.createElement('li', document.getElementById('amigo').value);
-        //listarNombres('listaAmigos', document.getElementById('amigo').value);
+        
         listarNombres();
     }
-    limpiarInput();
-    // console.log(nombreAmigos.length());
+    document.querySelector('#amigo').value = "";
+    
     console.log(listaNombreAmigos);
 }
 
-// Verifico si se presiona la tecla Enter 
+
+// Verifico si se presiona la tecla Enter ya que es mas facil que darle al boton para agregar
 document.getElementById('amigo').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         agregarAmigo();
@@ -36,6 +36,22 @@ function alerta(mensaje) {
     alert(mensaje);
 }
 
+//Esta funcion creara la lista de amigos que se han introduccido.
+function listarNombres() {
+    let lista = document.getElementById('listaAmigos');
+    // Esto Limpiara la lista antes de agregar los nuevos elementos
+    lista.innerHTML = "";
+
+    // recorro la lista  de amigos y agregar un <li> por cada uno
+    for (let i = listaNombreAmigos.length; i >= 0; i--) {
+        let li = document.createElement('li');
+        li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
+        lista.appendChild(li);  // esto para agregar  el <li> a la lista <ul>
+    }
+
+}
+
+//Esta es la funcion para sortear el amigo secreto
 function sortearAmigo() {
     let longitudArray = listaNombreAmigos.length;
     let numeroAleatorio = 0;
@@ -58,40 +74,6 @@ function sortearAmigo() {
     return;
 }
 
-function limpiarInput() {
-    document.querySelector('#amigo').value = "";
-}
 
 
 
-// function listarNombres(AmigoSecreto) {
-//     let lista = document.getElementById('listaAmigos');
-//     // Limpiar la lista antes de agregar los nuevos elementos
-//     lista.innerHTML = "";
-//     if(AmigoSecreto == ""){
-//         // Iterar sobre el arreglo de amigos y agregar un <li> por cada uno
-//         for (let i = 0; i < listaNombreAmigos.length; i++) {
-//             let li = document.createElement('li');  
-//             li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
-//             lista.appendChild(li);  // esto para agregar  el <li> a la lista <ul>
-//         }
-//     }else{
-//         let li = document.createElement('li');
-//         li.textContent = AmigoSecreto;
-//         lista.appendChild(li);
-//     }
-
-// }
-function listarNombres() {
-    let lista = document.getElementById('listaAmigos');
-    // Limpiar la lista antes de agregar los nuevos elementos
-    lista.innerHTML = "";
-
-    // recorro la lista  de amigos y agregar un <li> por cada uno
-    for (let i = listaNombreAmigos.length; i >= 0; i--) {
-        let li = document.createElement('li');
-        li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
-        lista.appendChild(li);  // esto para agregar  el <li> a la lista <ul>
-    }
-
-}
