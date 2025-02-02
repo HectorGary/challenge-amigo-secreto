0a85162036181dadd52e6aff13bb17298839a3d9
Hector Gary <hector-gary@hotmail.com>
2025-02-01 23:44:00 -0400

    Creo que ordené un poco mejor el código. Saqué la porción de código que mostraba al amigo sorteado, la cual se encontraba dentro del método listarAmigos, y la coloqué en sortearAmigo. Además, modifiqué la función listarNombres para que mostrara los nombres más recientes al principio de la lista.

--- app.js
+++ app.js
@@ -5,46 +5,52 @@ let listaNombreAmigos = [];
 let lista = document.getElementById('listaAmigos');
 
 // Esta funcion es para capturar y agregar el nombre del nuevo amigo.
-function agregarAmigo(){
-    if(document.getElementById('amigo').value == "" || listaNombreAmigos.includes(document.getElementById('amigo').value)){
-        if(document.getElementById('amigo').value == ""){
+function agregarAmigo() {
+    if (document.getElementById('amigo').value == "" || listaNombreAmigos.includes(document.getElementById('amigo').value)) {
+        if (document.getElementById('amigo').value == "") {
             alerta("Por favor, inserte un nombre.");
         }
-        if(listaNombreAmigos.includes(document.getElementById('amigo').value)){
+        if (listaNombreAmigos.includes(document.getElementById('amigo').value)) {
             alerta(`No puedes agregar de nuevo a ${document.getElementById('amigo').value}, ya lo agregaste antes`);
         }
-    } else{
-        listaNombreAmigos.push(document.getElementById('amigo').value);    
+    } else {
+        listaNombreAmigos.push(document.getElementById('amigo').value);
         //document.createElement('li', document.getElementById('amigo').value);
         //listarNombres('listaAmigos', document.getElementById('amigo').value);
-        listarNombres("");
-    } 
+        listarNombres();
+    }
     limpiarInput();
-   // console.log(nombreAmigos.length());
+    // console.log(nombreAmigos.length());
     console.log(listaNombreAmigos);
 }
 
 // Verifico si se presiona la tecla Enter 
-document.getElementById('amigo').addEventListener('keydown', function(event) {
+document.getElementById('amigo').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-      agregarAmigo();
+        agregarAmigo();
     }
 });
 
 //Esta funcion mostrara mensajes de alerta segun se requieran.
-function alerta(mensaje){
+function alerta(mensaje) {
     alert(mensaje);
 }
 
-function sortearAmigo(){
+function sortearAmigo() {
     let longitudArray = listaNombreAmigos.length;
     let numeroAleatorio = 0;
+    let lista = document.getElementById('listaAmigos');
 
-    if(listaNombreAmigos.length != 0){
-        numeroAleatorio = Math.floor(Math.random()*longitudArray);
-        
-        listarNombres(listaNombreAmigos[numeroAleatorio]);
-    }else{
+    if (listaNombreAmigos.length != 0) {
+        numeroAleatorio = Math.floor(Math.random() * longitudArray);
+
+        lista.innerHTML = "";
+        //listarNombres(listaNombreAmigos[numeroAleatorio]);
+
+        let li = document.createElement('li');
+        li.textContent = listaNombreAmigos[numeroAleatorio];
+        lista.appendChild(li);
+    } else {
         alerta("Primero deves agregar algunos amigos a la lista");
     }
     console.log(longitudArray);
@@ -52,41 +58,40 @@ function sortearAmigo(){
     return;
 }
 
-function limpiarInput(){
+function limpiarInput() {
     document.querySelector('#amigo').value = "";
 }
 
-/*function listarNombres(elemento, texto) {
-    let elementoHTML = document.querySelector(`#${elemento}`);
-    elementoHTML.innerHTML = texto;
-    return;
-}
 
-function listarNombres(){
-    for (let i = 0; i < listaNombreAmigos.length; i++) {
-        const element = array[i];
-        document.querySelector('#listaAmigos');
-    }
 
-    return;
-}
-*/
-function listarNombres(AmigoSecreto) {
+// function listarNombres(AmigoSecreto) {
+//     let lista = document.getElementById('listaAmigos');
+//     // Limpiar la lista antes de agregar los nuevos elementos
+//     lista.innerHTML = "";
+//     if(AmigoSecreto == ""){
+//         // Iterar sobre el arreglo de amigos y agregar un <li> por cada uno
+//         for (let i = 0; i < listaNombreAmigos.length; i++) {
+//             let li = document.createElement('li');  
+//             li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
+//             lista.appendChild(li);  // esto para agregar  el <li> a la lista <ul>
+//         }
+//     }else{
+//         let li = document.createElement('li');
+//         li.textContent = AmigoSecreto;
+//         lista.appendChild(li);
+//     }
+
+// }
+function listarNombres() {
     let lista = document.getElementById('listaAmigos');
     // Limpiar la lista antes de agregar los nuevos elementos
     lista.innerHTML = "";
-    if(AmigoSecreto == ""){
-        // Iterar sobre el arreglo de amigos y agregar un <li> por cada uno
-        for (let i = 0; i < listaNombreAmigos.length; i++) {
-            let li = document.createElement('li');  // Crear un elemento <li>
-            li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
-            lista.appendChild(li);  // Agregar el <li> a la lista <ul>
-        }
-    }else{
-        let li = document.createElement('li');
-        li.textContent = AmigoSecreto;
-        lista.appendChild(li);
-    }
-    
-  }
 
+    // recorro la lista  de amigos y agregar un <li> por cada uno
+    for (let i = listaNombreAmigos.length; i >= 0; i--) {
+        let li = document.createElement('li');
+        li.textContent = listaNombreAmigos[i];  // Asignar el nombre del amigo al <li>
+        lista.appendChild(li);  // esto para agregar  el <li> a la lista <ul>
+    }
+
+}
